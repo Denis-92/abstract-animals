34ca64c6d434d58411eed322133a77c1a64ea698
Denis-92 <d.mozenic@gmail.com>
2023-02-01 16:52:56 +0100

    add Azione class methods

--- src/org/lessons/java/animals/Azione.java
+++ src/org/lessons/java/animals/Azione.java
@@ -2,4 +2,11 @@ package org.lessons.java.animals;
 
 public class Azione {
 
+	public void faiVolare (Ivolante animale) {
+		animale.vola();
+	}
+	
+	public void faiNuotare (Inuotante animale) {
+		animale.nuota();
+	}
 }
